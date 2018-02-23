b92ebd28a7ff15cf719dfd618735ddcddbb167b7
Imrayya <imrayya@gmail.com>
2018-02-23 22:39:13 +0100

    Hello World

---
+++ Chess/Point2D.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Chess
+{
+    public struct Point2D
+    {
+        private byte _x;
+        private byte _y;
+
+        public byte X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public byte Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public Point2D(Point2D point2D)
+        {
+            _x = point2D._x;
+            _y = point2D._y;
+        }
+
+        public Point2D(byte x, byte y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public Point2D(string coor)
+        {
+            _x = Util.ConvertLetterToNumber(coor[0]) ?? throw new ArgumentException("Invalid Coordiantes");
+            _y = byte.Parse(coor[1].ToString());
+
+        }
+
+        public static Point2D operator +(Point2D a, Point2D b)
+        {
+            var x = a._x + b._x;
+            var y = a._y + b._y;
+            return new Point2D((byte) x, (byte) y);
+        }
+    }
+}
---
+++ Chess/Util.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Chess
+{
+    public class Util
+    {
+        public static byte? ConvertLetterToNumber(char c)
+        {
+            switch (c)
+            {
+                case 'a': return 0;
+                case 'b': return 1;
+                case 'c': return 2;
+                case 'd': return 3;
+                case 'e': return 4;
+                case 'f': return 5;
+                case 'g': return 6;
+                case 'h': return 7;
+                default: return null;
+            }
+        }
+
+        public static char? ConvertNumberToLetter(Byte b)
+        {
+            switch (b)
+            {
+                case 0: return 'a';
+                case 1: return 'b';
+                case 2: return 'c';
+                case 3: return 'd';
+                case 4: return 'e';
+                case 5: return 'f';
+                case 6: return 'g';
+                case 7: return 'h';
+                default: return null;
+            }
+        }
+    }
+}
